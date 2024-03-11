217cb71998e4941a924ef48560ae618c15d2e7f0
Eric Springer <ericspringer@outlook.com>
2024-03-11 19:21:14 -0300

    correcoes na logica de calculo

---
+++ javascript/calculo.js
@@ -0,0 +1,76 @@
+const digitos = document.querySelectorAll('.number')
+const visor = document.getElementById('visor')
+const clear = document.getElementById('clear')
+const operacoes = document.querySelectorAll('.operacao')
+const igual = document.getElementById('igual')
+const corBg = document.querySelectorAll('.cor')
+const bodyHtml = document.querySelector('body')
+const resultadoSpan = document.createElement('span')
+
+digitos.forEach(function(digito) {
+    digito.addEventListener('click', function() {
+
+        // const novoDigito = document.createElement('span')
+        // novoDigito.innerText = digito.innerText
+
+        resultadoSpan.innerText = ''
+
+        visor.append(digito.innerText)
+
+    })
+})
+
+clear.addEventListener('click', function() {
+    visor.innerText = ''
+})
+
+operacoes.forEach(function(operacao) {
+    operacao.addEventListener('click', function() {
+        simbolo(operacao.innerText)
+    })
+}) 
+
+function simbolo(operacao) {
+    var adicao = false;
+    var subtracao = false;
+    var num1, num2, resultado;
+
+    switch (operacao) {
+        case '+':
+            num1 = parseInt(visor.innerText);
+            console.log(num1)
+            visor.innerText = '';
+            adicao = true;
+            break;
+
+        case '-':
+            alert('Trabalho em progresso..')
+            break;
+        case 'X':
+            alert('Trabalho em progresso')
+            break
+
+        case '/':
+            alert('Trabalho em progresso');
+            break
+
+        case '%':
+            alert('Trabalho em progresso')
+            
+    }
+
+    if (adicao) {
+        igual.addEventListener('click', function () {
+            
+            num2 = parseInt(visor.innerText);
+            resultado = num1 + num2;
+            visor.innerText = '';
+            resultadoSpan.innerText = resultado;
+            visor.appendChild(resultadoSpan);
+            num1 = '';
+            num2 = '';
+            adicao = false;
+        });
+
+    }
+}
---
+++ javascript/utilidadesCalc.js
@@ -0,0 +1,32 @@
+const calculadoraWorspace = document.getElementById('calculadoraAbrir')
+const calculadoraReal = document.getElementById('Calculadora')
+const calculadoraFechar = document.querySelector('.utilidadeItem')
+const calcUtil = document.getElementById('utilidades')
+let mousePressionado = false
+
+calcUtil.addEventListener('mousedown', function() {
+    mousePressionado = true
+    console.log(mousePressionado)
+})
+
+calcUtil.addEventListener('mouseup', function() {
+    mousePressionado = false
+    console.log(mousePressionado)
+})
+
+calculadoraReal.addEventListener('mousemove', (e) => {
+    if(mousePressionado == true) {
+
+        calculadoraReal.style.left = (e.pageX - calculadoraReal.clientWidth / 2) + 'px'
+        calculadoraReal.style.top = (e.pageY - calculadoraReal.clientHeight / 15) + 'px'
+    }
+})
+
+calculadoraWorspace.addEventListener('dblclick', function() {
+    calculadoraReal.style.visibility = 'visible'
+})
+
+calculadoraFechar.addEventListener('click', function() {
+    mousePressionado = false
+    calculadoraReal.style.visibility = 'hidden'
+})
